d33564404657731c10e0c262cc0719eb108ea946
Ted Marshall <ted@tedm.net>
2014-09-09 21:53:16 -0700

    Updated CPTs
    • Consolidated registering of CPTs to a single file in Library
    • Rearranged the order in which the CPTs appear in the Admin sidebar
    • Added CPT icons
    • Deactivated unnecessary Plug-ins

--- library/admin.php
+++ library/admin.php
@@ -15,58 +15,6 @@ Digging into WP - http://digwp.com/2010/10/customize-wordpress-dashboard/
 
 */
 
-/************* DASHBOARD WIDGETS *****************/
-
-// disable default dashboard widgets
-function disable_default_dashboard_widgets() {
-	// remove_meta_box('dashboard_right_now', 'dashboard', 'core');    // Right Now Widget
-	remove_meta_box('dashboard_recent_comments', 'dashboard', 'core'); // Comments Widget
-	remove_meta_box('dashboard_incoming_links', 'dashboard', 'core');  // Incoming Links Widget
-	remove_meta_box('dashboard_plugins', 'dashboard', 'core');         // Plugins Widget
-
-	// remove_meta_box('dashboard_quick_press', 'dashboard', 'core');  // Quick Press Widget
-	remove_meta_box('dashboard_recent_drafts', 'dashboard', 'core');   // Recent Drafts Widget
-	remove_meta_box('dashboard_primary', 'dashboard', 'core');         //
-	remove_meta_box('dashboard_secondary', 'dashboard', 'core');       //
-
-	// removing plugin dashboard boxes
-	remove_meta_box('yoast_db_widget', 'dashboard', 'normal');         // Yoast's SEO Plugin Widget
-
-	/*
-	have more plugin widgets you'd like to remove?
-	share them with us so we can get a list of
-	the most commonly used. :D
-	https://github.com/eddiemachado/bones/issues
-	*/
-}
-
-/*
-Now let's talk about adding your own custom Dashboard widget.
-Sometimes you want to show clients feeds relative to their
-site's content. For example, the NBA.com feed for a sports
-site. Here is an example Dashboard Widget that displays recent
-entries from an RSS Feed.
-
-For more information on creating Dashboard Widgets, view:
-http://digwp.com/2010/10/customize-wordpress-dashboard/
-*/
-
-// RSS Dashboard Widget
-
-// calling all custom dashboard widgets
-// function bones_custom_dashboard_widgets() {
-// 	wp_add_dashboard_widget('bones_rss_dashboard_widget', __('Recently on Themble (Customize on admin.php)', 'rsblawtheme'), 'bones_rss_dashboard_widget');
-	/*
-	Be sure to drop any other created Dashboard Widgets
-	in this function and they will all load.
-	*/
-// }
-
-
-// removing the dashboard widgets
-add_action('admin_menu', 'disable_default_dashboard_widgets');
-// adding any custom widgets
-//add_action('wp_dashboard_setup', 'bones_custom_dashboard_widgets');
 
 
 /************* CUSTOM LOGIN PAGE *****************/
--- library/results-custom_post_type.php
+++ library/results-custom_post_type.php
@@ -1,52 +1,105 @@
 <?php
-/* Legal Results Custom Post Type
+/* Custom Post Types, Categories, and Taxonomies
+
 */
 
-if ( ! function_exists('rsb_result_cpt') ) {
+// Flush rewrite rules for custom post types
+add_action( 'after_switch_theme', 'bones_flush_rewrite_rules' );
 
-// Register Custom Post Type
+// Flush your rewrite rules
+function bones_flush_rewrite_rules() {
+	flush_rewrite_rules();
+}
+
+// let's create the function for the Post Types
 function rsb_result_cpt() {
-
-    $labels = array(
-        'name'                => _x( 'Results', 'Post Type General Name', 'rsblawtheme' ),
-        'singular_name'       => _x( 'Result', 'Post Type Singular Name', 'rsblawtheme' ),
-        'menu_name'           => __( 'Results', 'rsblawtheme' ),
-        'parent_item_colon'   => __( 'Parent Result:', 'rsblawtheme' ),
-        'all_items'           => __( 'All Results', 'rsblawtheme' ),
-        'view_item'           => __( 'View Result', 'rsblawtheme' ),
-        'add_new_item'        => __( 'Add New Result', 'rsblawtheme' ),
-        'add_new'             => __( 'Add New', 'rsblawtheme' ),
-        'edit_item'           => __( 'Edit Result', 'rsblawtheme' ),
-        'update_item'         => __( 'Update Result', 'rsblawtheme' ),
-        'search_items'        => __( 'Search Results', 'rsblawtheme' ),
-        'not_found'           => __( 'Not found', 'rsblawtheme' ),
-        'not_found_in_trash'  => __( 'Not found in Trash', 'rsblawtheme' ),
-    );
-    $results_args = array(
-        'label'               => __( 'rsb_result', 'rsblawtheme' ),
-        'description'         => __( 'Results from Previous Cases.', 'rsblawtheme' ),
-        'labels'              => $labels,
-        'supports'            => array( 'title', 'editor' ),
-        'taxonomies'          => array( 'category', 'post_tag' ),
-        'hierarchical'        => true,
-        'public'              => true,
-        'show_ui'             => true,
-        'show_in_menu'        => true,
-        'show_in_nav_menus'   => true,
-        'show_in_admin_bar'   => true,
-        'menu_position'       => 6, /* Appears right after Articles & Decisions (at 5), which is right after Posts */
-        'menu_icon' => get_stylesheet_directory_uri() . '/library/images/custom-post-icon.png', /* the icon for the custom post type menu */
-        'can_export'          => true,
-        'has_archive'         => true,
-        'exclude_from_search' => false,
-        'publicly_queryable'  => true,
-        'capability_type'     => 'post',
-    );
-    register_post_type( 'rsb_result', $results_args );
-
+	
+	// *******************
+	// Registering RESULTS 
+	// *******************
+	register_post_type( 'rsb_result', /* (http://codex.wordpress.org/Function_Reference/register_post_type) */
+		// let's now add all the options for this post type
+		array( 'labels' => array(
+			'name' => __( 'Results', 'bonestheme' ), /* This is the Title of the Group */
+			'singular_name' => __( 'Result', 'bonestheme' ), /* This is the individual type */
+			'all_items' => __( 'All Results', 'bonestheme' ), /* the all items menu item */
+			'add_new' => __( 'Add New', 'bonestheme' ), /* The add new menu item */
+			'add_new_item' => __( 'Add New Result', 'bonestheme' ), /* Add New Display Title */
+			'edit' => __( 'Edit', 'bonestheme' ), /* Edit Dialog */
+			'edit_item' => __( 'Edit Results', 'bonestheme' ), /* Edit Display Title */
+			'new_item' => __( 'New Result', 'bonestheme' ), /* New Display Title */
+			'view_item' => __( 'View Result', 'bonestheme' ), /* View Display Title */
+			'search_items' => __( 'Search Result', 'bonestheme' ), /* Search Partner Title */ 
+			'not_found' =>  __( 'Nothing found in the Database.', 'bonestheme' ), /* This displays if there are no entries yet */ 
+			'not_found_in_trash' => __( 'Nothing found in Trash', 'bonestheme' ), /* This displays if there is nothing in the trash */
+			'parent_item_colon' => ''
+			), /* end of arrays */
+			'description' => __( 'Media coverage of decisions and other articles', 'bonestheme' ), /* Partner Description */
+			'public' => false,
+			'publicly_queryable' => true,
+			'exclude_from_search' => false,
+			'show_ui' => true,
+			'query_var' => true,
+			'menu_position' => 21, /* this is what order you want it to appear in on the left hand side menu */ 
+			'menu_icon' => get_stylesheet_directory_uri() . '/library/images/icon-results.png', /* the icon for the custom post type menu */
+			'capability_type' => 'post'
+		) /* end of options */
+	); /* end of register Results */
+	
+	
+	// ******************************************
+	// Registering PUBLISHED ARTICLES & DECISIONS 
+	// ******************************************
+	register_post_type( 'published_articles', /* (http://codex.wordpress.org/Function_Reference/register_post_type) */
+		// let's now add all the options for this post type
+		array( 'labels' => array(
+			'name' => __( 'Articles/Decisions', 'bonestheme' ), /* This is the Title of the Group */
+			'singular_name' => __( 'Article/Decision', 'bonestheme' ), /* This is the individual type */
+			'all_items' => __( 'All Articles/Decisions', 'bonestheme' ), /* the all items menu item */
+			'add_new' => __( 'Add New', 'bonestheme' ), /* The add new menu item */
+			'add_new_item' => __( 'Add New Article/Decision', 'bonestheme' ), /* Add New Display Title */
+			'edit' => __( 'Edit', 'bonestheme' ), /* Edit Dialog */
+			'edit_item' => __( 'Edit Articles/Decisions', 'bonestheme' ), /* Edit Display Title */
+			'new_item' => __( 'New Article/Decision', 'bonestheme' ), /* New Display Title */
+			'view_item' => __( 'View Article/Decision', 'bonestheme' ), /* View Display Title */
+			'search_items' => __( 'Search Article/Decision', 'bonestheme' ), /* Search Partner Title */ 
+			'not_found' =>  __( 'Nothing found in the Database.', 'bonestheme' ), /* This displays if there are no entries yet */ 
+			'not_found_in_trash' => __( 'Nothing found in Trash', 'bonestheme' ), /* This displays if there is nothing in the trash */
+			'parent_item_colon' => ''
+			), /* end of arrays */
+			'description' => __( 'Media coverage of decisions and other articles', 'bonestheme' ), /* Partner Description */
+			'public' => false,
+			'publicly_queryable' => true,
+			'exclude_from_search' => false,
+			'show_ui' => true,
+			'query_var' => true,
+			'menu_position' => 22, /* this is what order you want it to appear in on the left hand side menu */ 
+			'menu_icon' => get_stylesheet_directory_uri() . '/library/images/icon-articles.png', /* the icon for the custom post type menu */
+			'taxonomies' => array('category', 'post_tag'), /* displays categories and tags for the cpt */
+			'capability_type' => 'post'
+		) /* end of options */
+	); /* end of register Published Articles and Decisions */
+	
+	
+	// ****************************************************
+	// Add the default Categories or Tags to the post types 
+	// ****************************************************
+	/* this adds your post categories to your custom post type */
+	// register_taxonomy_for_object_type( 'category', 'boardmember' );
+	/* this adds your post tags to your custom post type */
+	// register_taxonomy_for_object_type( 'post_tag', 'boardmember' );
+	
 }
 
-// Hook into the 'init' action
-add_action( 'init', 'rsb_result_cpt', 0 );
 
-}
+	// *****************************************
+	// adding the function to the Wordpress init 
+	// *****************************************
+	add_action( 'init', 'rsb_result_cpt');
+	
+	/*
+	for more information on taxonomies, go here:
+	http://codex.wordpress.org/Function_Reference/register_taxonomy
+	*/
+
+?>
