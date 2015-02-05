eff017717e386cf13fe7dc4d90b13b35c09cfa46
Ted Marshall <ted@tedm.net>
2015-02-05 07:43:29 -0800

    -fix cleaned up header & footer javascript
    • added analytics
    • removed live reload js

--- header.php
+++ header.php
@@ -36,12 +36,20 @@
 
 	<link href='http://fonts.googleapis.com/css?family=Merriweather:400,700,700italic' rel='stylesheet' type='text/css'>
 	<!-- <link href='http://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'> -->
-	<!-- drop Google Analytics Here -->
-
-	<!-- end analytics -->
 </head>
 
 <body <?php body_class(); ?>>
+	<?php // drop Google Analytics Here ?>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-55770513-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
+	<?php // end analytics ?>
 	<div id="container">
 		<header class="header" role="banner">
 
