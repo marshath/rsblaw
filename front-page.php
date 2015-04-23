9d2efb8f7c255ade14d4905892a6bb5211373cd8
Ted Marshall <ted@tedm.net>
2015-04-23 10:03:33 -0700

    -fix mobile CTA hero image CSS
    • remove .cta-hero background image on mobile

--- front-page.php
+++ front-page.php
@@ -13,15 +13,15 @@ Template Name: Custom Front Page (Home) Template
 					the_post(); ?>
 							
 			<div class="cta-hero" style="background-image:url(<?php the_field('cta_image')?>);">
-				
-				<div class="cta-container"><img src="<?php the_field('cta_image')?>" class="cta-image"></div> <!--.cta-container -->
-				
-				<div class="cta-textbox">
-					<h1><em><?php the_field('cta_heading'); ?></em></h1>
-					<p><?php the_field('cta_body'); ?></p>
-					<a href="<?php the_field('cta_link'); ?>"><div class="button-primary aligncenter"><?php the_field('cta_link_label'); ?></div></a>
-				</div> <!-- .cta-textbox -->
-				
+				<span>
+					<div class="cta-container"><img src="<?php the_field('cta_image')?>" class="cta-image"></div> <!--.cta-container -->
+					
+					<div class="cta-textbox">
+						<h1><em><?php the_field('cta_heading'); ?></em></h1>
+						<p><?php the_field('cta_body'); ?></p>
+						<a href="<?php the_field('cta_link'); ?>"><div class="button-primary aligncenter"><?php the_field('cta_link_label'); ?></div></a>
+					</div> <!-- .cta-textbox -->
+				</span>
 			</div>
 			
 			<div id="inner-content" class="wrap clearfix">
